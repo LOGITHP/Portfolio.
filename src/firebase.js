b8e847c23f6613804be6c2f4d1b7d5d883b04552
LOGITHP <logithp5736@gmail.com>
2026-06-30 16:38:40 +0530

    edited firebase.jsom

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-  databaseURL: "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
-  projectId: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-  storageBucket: "zzzzzzzzzzzzzzzzzzzzzzzzzzzz",
-  messagingSenderId: "ffffffffffffffffffffffffffffffffff",
-  appId: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-  measurementId: "eeeeeeeeeeeeeeeeeeeeeeee"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
