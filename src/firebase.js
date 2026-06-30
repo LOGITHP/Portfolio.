d0077f7892c9a63e773b1bdc1c2d270f73ce21ec
LOGITHP <logithp5736@gmail.com>
2026-06-30 16:27:03 +0530

    final

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAVybkIbphKrU6xq2cdX0iE6MK-04UEKs",
-  authDomain: "portfolio-67769.firebaseapp.com",
-  databaseURL: "https://portfolio-67769-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "portfolio-67769",
-  storageBucket: "portfolio-67769.firebasestorage.app",
-  messagingSenderId: "723505469304",
-  appId: "1:723505469304:web:02999bb2e9ae30c38eda8d",
-  measurementId: "G-ZSN66XQ4LT"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+  databaseURL: "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
+  projectId: "zzzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+  storageBucket: "zzzzzzzzzzzzzzzzzzzzzzzzzzzz",
+  messagingSenderId: "ffffffffffffffffffffffffffffffffff",
+  appId: "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+  measurementId: "eeeeeeeeeeeeeeeeeeeeeeee"
 };
 
 const app = initializeApp(firebaseConfig);
